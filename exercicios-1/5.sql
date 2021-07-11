c565a6470753b34cc4a7b84976b11c1d110d2856
RonaldoSandim <ronaldosandim19@gmail.com>
2021-07-11 20:33:48 -0300

    Renomeei todos os arquivos

---
+++ exercicios-1/5.sql
@@ -0,0 +1,6 @@
+
+-- 5. Consultando a tabela empregados (employees), selecione os nomes (first_name) distintos. Ordene pelo
+-- campo nome (first_name) de forma descendente.
+
+SELECT DISTINCT first_name FROM employees
+order by first_name desc;
